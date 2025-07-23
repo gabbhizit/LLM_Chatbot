f949f7b3f24c987d50017987b01d306cc3ec1cc2
Abbhizit GT <gabbhizit@gmail.com>
2025-07-23 23:16:22 +0530

    commit

--- main.py
+++ main.py
@@ -22,18 +22,3 @@ if submit_button and youtube_url and query:
     response= yth.get_response_from_query(db,query)
     st.subheader("Answer:")
     st.text(textwrap.fill(response, width=80))
-
-
-
-
-# Uncomment the following lines to run the langchain helper functionality
-"""st.title("Cute Pet Name Generator")
-
-animal_type = st.sidebar.selectbox(
-    "Select an animal type",("cat", "dog", "cow", "pig", "sheep"))
-
-if animal_type:
-    with st.spinner("Generating cute pet names..."):
-        names = lch.gen_pet_name(animal_type)
-        st.write(f"Here are some cute {animal_type} names:")
-        st.write(names)"""
