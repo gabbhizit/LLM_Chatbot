f57b53b1d23ae3b10222b1efa15e49dcba75b38b
Abbhizit GT <gabbhizit@gmail.com>
2025-07-23 23:24:28 +0530

    reusable card

---
+++ codes.py
@@ -0,0 +1,12 @@
+# Uncomment the following lines to run the langchain helper functionality
+"""st.title("Cute Pet Name Generator")
+
+animal_type = st.sidebar.selectbox(
+    "Select an animal type",("cat", "dog", "cow", "pig", "sheep"))
+
+if animal_type:
+    with st.spinner("Generating cute pet names..."):
+        names = lch.gen_pet_name(animal_type)
+        st.write(f"Here are some cute {animal_type} names:")
+        st.write(names)"""
+
